7338a05419785c365e539923210176b6242233c7
Projects2019 <git.project2019@gmail.com>
2019-03-18 03:55:18 +0530

    sign up and login done

---
+++ app/src/main/java/com/fixers/coolspot/SignUp.java
@@ -0,0 +1,13 @@
+package com.fixers.coolspot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
