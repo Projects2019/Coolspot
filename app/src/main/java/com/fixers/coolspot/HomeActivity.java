5e78774b3081d66a44a19ff851dde4d69db1491a
Projects2019 <git.project2019@gmail.com>
2019-03-17 16:01:31 +0530

    homepage created

--- app/src/main/java/com/fixers/coolspot/HomeActivity.java
+++ app/src/main/java/com/fixers/coolspot/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.fixers.coolspot;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -16,12 +18,27 @@ import android.view.MenuItem;
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static int SPLASH_TIME_OUT = 4000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+      /*  new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(HomeActivity.this,WelcomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT); */
+
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/fixers/coolspot/HotDeals.java
@@ -0,0 +1,4 @@
+package com.fixers.coolspot;
+
+public class HotDeals {
+}
